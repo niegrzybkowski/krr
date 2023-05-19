5dc5286cd93b7978a7d6f79f66279527f1ea9e06
cinekele <cinekele@gmail.com>
2023-05-19 23:13:34 +0200

    imports

--- backend/base/__init__.py
+++ backend/base/__init__.py
@@ -1,7 +1,4 @@
 from __future__ import annotations
-import os,sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 from .exception import BackendException, ParsingException, LogicException
 from .agent import Agent
